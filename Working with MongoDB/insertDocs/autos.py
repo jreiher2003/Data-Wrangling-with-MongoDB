ebcfa8ed2fc748771dabc72d0db97f441d3eaeab
Jeff Reiher <jeffreiher@gmail.com>
2015-05-17 18:24:15 -0400

    way to buggy for me to work on

--- Working with MongoDB/insertDocs/autos.py
+++ Working with MongoDB/insertDocs/autos.py
@@ -3,10 +3,11 @@ import csv
 import json
 import io
 import re
-from pprint import pprint
+import pprint
+
 
 field_map = {
-	"name" : "name",
+    "name" : "name",
     "bodyStyle_label" : "bodyStyle",
     "assembly_label" : "assembly",
     "class_label" : "class",
@@ -26,139 +27,155 @@ field_map = {
     "productionEndYear" : "productionEndYear",
     "transmission" : "transmission"
 }
-
 fields = field_map.keys()
 
+
 def skip_lines(input_file, skip):
-	for i in range(0, skip):
-		next(input_file)
+    for i in range(0, skip):
+        next(input_file)
 
 def is_number(s):
-	try:
-		float(s)
-		return True
-	except ValueError:
-		return False
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 def strip_automobile(v):
-	return re.sub(r"\s*\(automobile\)\s*", " ", v)
+    return re.sub(r"\s*\(automobile\)\s*", " ", v)
 
 def strip_city(v):
-	return re.sub(r"\s*\(city\)\s*", " ", v)
+    return re.sub(r"\s*\(city\)\s*", " ", v)
 
 def parse_array(v):
-	if (v[0] == "{") and (v[-1] == "}"):
-		v = v.lstrip("{")
-		v = v.rstrip("}")
-		v_array = v.split("|")
-		v_array = [i.strip() for i in v_array]
-		return v_array
-	return v
+    if (v[0] == "{") and (v[-1] == "}"):
+        v = v.lstrip("{")
+        v = v.rstrip("}")
+        v_array = v.split("|")
+        v_array = [i.strip() for i in v_array]
+        return v_array
+    return v
 
 def mm_to_meters(v):
-	if v < .01:
-		return v * 1000
-	return v
+    if v < 0.01:
+        return v * 1000
+    return v
 
 def clean_dimension(d, field, v):
-	if is_number(v):
-		if field == 'weight':
-			d[field] = float(v) / 1000.0
-		else:
-			d[field] = mm_to_meters(float(v))
-
+    if is_number(v):
+        if field == "weight":
+            d[field] = float(v) / 1000.0
+        else:
+            d[field] = mm_to_meters(float(v))
+    
 def clean_year(d, field, v):
-	d[field] = v[0:4]
+    d[field] = v[0:4]
 
 def parse_array2(v):
-	if (v[0] == "{") and (v[-1] == "}"):
-		v = v.lstrip("{")
-		v = v.rstript("}")
-		v_array = v.split("|")
-		v_array = [i.strip() for i in v_array]
-		return (True, v_array)
-	return (False, v)
+    if (v[0] == "{") and (v[-1] == "}"):
+        v = v.lstrip("{")
+        v = v.rstrip("}")
+        v_array = v.split("|")
+        v_array = [i.strip() for i in v_array]
+        return (True, v_array)
+    return (False, v)
 
 def ensure_not_array(v):
-	(is_array, v) = parse_array(v)
-	if is_array:
-		return v[0]
-	return v 
+    (is_array, v) = parse_array(v)
+    if is_array:
+        return v[0]
+    return v
 
 def ensure_array(v):
-	(is_array, v) = parse_array2(v)
-	if is_array:
-		return v
-	return [v]
+    (is_array, v) = parse_array2(v)
+    if is_array:
+        return v
+    return [v]
 
 def ensure_float(v):
-	if is_number(v):
-		return float(v)
+    if is_number(v):
+        return float(v)
 
 def ensure_int(v):
-	if is_number(v):
-		return int(v)
+    if is_number(v):
+        return int(v)
 
 def ensure_year_array(val):
-	vals = ensure_array(val)
-	year_vals = []
-	for v in vals:
-		v = v[0:4]
-		v = int(v)
-		if v:
-			year_vals.append(v)
-	return year_vals
+    #print "val:", val
+    vals = ensure_array(val)
+    year_vals = []
+    for v in vals:
+        v = v[0:4]
+        v = int(v)
+        if v:
+            year_vals.append(v)
+    return year_vals
 
-def empty_val(vals):
-	val = val.strip()
-	return (val == "NULL") or (val == "")
+def empty_val(val):
+	if val == "":
+		return ""
+	elif val == "NULL":
+		return "NULL"
+	else:
+		return val
+    # val = str(0 if val is None else val)
+    # val = val.strip()
+    # return (val == "NULL") or (val == "")
 
 def years(row, start_field, end_field):
-	start_val = row[start_field]
-	end_val = row[end_field]
+    start_val = row[start_field]
+    end_val = row[end_field]
 
-	if empty_val(start_val) or empty_val(end_val):
-		return []
+    if empty_val(start_val) or empty_val(end_val):
+        return []
 
-	start_years = ensure_year_array(start_val)
-	if start_years:
-		start_years = sorted(start_years)
-	end_years = ensure_year_array(end_val)
-	if end_years:
-		end_years = sorted(end_years)
-	all_years = []
-	if start_years and end_years:
-		for i in range(0, min(len(start_years), len(end_years))):
-			for y in range(start_years[i], end_years[i] + 1):
-				all_years.append(y)
-	return all_years
+    start_years = ensure_year_array(start_val)
+    if start_years:
+        start_years = sorted(start_years)
+    end_years = ensure_year_array(end_val)
+    if end_years:
+        end_years = sorted(end_years)
+    all_years = []
+    if start_years and end_years:
+        #print start_years
+        #print end_years
+        for i in range(0, min(len(start_years), len(end_years))):
+            for y in range(start_years[i], end_years[i]+1):
+                all_years.append(y)
+    return all_years
+
 
 def process_file(input_file):
-	input_data = csv.DictReader(open(input_file))
-	autos = []
-	skip_lines(input_data,3)
-	for row in input_data:
-		auto = {}
-		model_years = {}
-		dimensions = {}
-		for field, val in row.iteritems():
-			if field not in fields or empty_val(val):
-				continue
-			if field in ["bodyStyle_label", "class_label", "layout_label"]:
-				val = val.lower()
-			val = strip_automobile(val)
-			val = strip_city(val)
-			val = val.strip()
-			val = parse_array(val)
-			if field in ["length", "width", "height", "weight", "wheelbase"]:
-				clean_dimension(dimensions, field_map[field], val)
-			elif field in ["productionStartYear", "productionEndYear"]:
-				clean_year(production_years, field_map[field], val)
-			else:
-				auto[field_map[field]] = val
-		if dimensions:
-			auto['dimensions'] = dimensions
-		auto['modelYears'] = years(row, 'modelStartYear', 'modelEndYear')
-		auto['productionYears'] = years(row, 'productionStartYear', 'productionEndYear')
-		auto.append(auto)
-	return autos
+    input_data = csv.DictReader(open(input_file))
+    autos = []
+    skip_lines(input_data, 3)
+    for row in input_data:
+        auto = {}
+        model_years = {}
+        production_years = {}
+        dimensions = {}
+        for field, val in row.iteritems():
+            if field not in fields: # or empty_val(val):
+                continue
+            if field in ["bodyStyle_label", "class_label", "layout_label"]:
+                # val = val.strip()
+                val = val.lower()
+            val = strip_automobile(val)
+            val = strip_city(val)
+            val = val.strip()
+            val = parse_array(val)
+            if field in ["length", "width", "height", "weight", "wheelbase"]:
+                clean_dimension(dimensions, field_map[field], val)
+            elif field in ["modelStartYear", "modelEndYear"]:
+                clean_year(model_years, field_map[field], val)
+            elif field in ["productionStartYear", "productionEndYear"]:
+                clean_year(production_years, field_map[field], val)
+            else:
+                auto[field_map[field]] = val
+        if dimensions:
+            auto['dimensions'] = dimensions
+        auto['modelYears'] = years(row, 'modelStartYear', 'modelEndYear')
+        auto['productionYears'] = years(row, 'productionStartYear', 'productionEndYear')
+        autos.append(auto)
+    return autos
+
