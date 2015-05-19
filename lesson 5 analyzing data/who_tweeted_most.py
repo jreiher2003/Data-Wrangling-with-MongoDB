672f8e1cd64945d7fd3163e1126c053237fbdfa8
Jeff Reiher <jeffreiher@gmail.com>
2015-05-19 15:02:06 -0400

    basic agg cmd to count user tweets

---
+++ lesson 5 analyzing data/who_tweeted_most.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+client = MongoClient("mongodb://localhost:27017")
+db = client.twitter
+
+def most_tweets():
+	result = db.tweets.aggregate([
+				{"$group": {"_id": "$user.screen_name", "count": {"$sum":1}}},
+				{"$sort": {"count": -1}}
+				])
+	return result
+
+if __name__ == '__main__':
+	result = most_tweets()
+	pprint(result)
